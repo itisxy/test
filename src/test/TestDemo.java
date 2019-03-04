279a7542c132ef1b657b068d1d54cf939c58d4d4
itisxy <48087017+itisxy@users.noreply.github.com>
2019-03-04 14:04:18 +0800

    update
    update 01

--- src/test/TestDemo.java
+++ src/test/TestDemo.java
@@ -3,6 +3,7 @@ package test;
 public class TestDemo {
 	public void test(){
 		System.out.println("aa");
+		System.out.println("bb");
 	}
 	public static void main(String[] args) {
 		new TestDemo().test();
